5fa56016c67dfdc9aca83773740e078af5d7b56d
Nuttapong Phisitbutr <nuttapong.p@nextzy.com>
2025-05-21 15:27:26 +0700

    Refactor HeroSection

--- apps/app/lib/src/presentations/home/components/hero_section.dart
+++ apps/app/lib/src/presentations/home/components/hero_section.dart
@@ -16,103 +16,7 @@ class HeroSection extends AppStatelessComponent {
       [
         _buildBackground1(),
         _buildBackground2(),
-        div(
-          classes:
-              'mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:px-8 lg:py-40',
-          [
-            div(
-              classes: 'mx-auto max-w-2xl shrink-0 lg:mx-0 lg:pt-8',
-              [
-                img(
-                  classes: 'h-11',
-                  src:
-                      'https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500',
-                  alt: 'Your Company',
-                ),
-                div(
-                  classes: 'mt-24 sm:mt-32 lg:mt-16',
-                  [
-                    a(
-                      classes: 'inline-flex space-x-6',
-                      href: '#',
-                      [
-                        span(
-                          classes:
-                              'rounded-full bg-indigo-500/10 px-3 py-1 text-sm/6 font-semibold text-indigo-400 ring-1 ring-indigo-500/20 ring-inset',
-                          [
-                            text("What's new"),
-                          ],
-                        ),
-                        span(
-                          classes:
-                              'inline-flex items-center space-x-2 text-sm/6 font-medium text-gray-300',
-                          [
-                            span([text('Just shipped v1.0')]),
-                            svg(
-                              classes: 'size-5 text-gray-500',
-                              viewBox: '0 0 20 20',
-                              attributes: {
-                                'fill': 'currentColor',
-                                'aria-hidden': 'true',
-                                'data-slot': 'icon',
-                              },
-                              [
-                                path(
-                                  attributes: {
-                                    'fill-rule': 'evenodd',
-                                    'clip-rule': 'evenodd',
-                                  },
-                                  d: 'M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z',
-                                  [],
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                h1(
-                  classes:
-                      'mt-10 text-5xl font-semibold tracking-tight text-pretty text-white sm:text-7xl',
-                  [text('Deploy to the cloud with confidence')],
-                ),
-                p(
-                  classes:
-                      'mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8',
-                  [
-                    text(
-                        'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.')
-                  ],
-                ),
-                div(
-                  classes: 'mt-10 flex items-center gap-x-6',
-                  [
-                    a(
-                      classes:
-                          'rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400',
-                      href: '#',
-                      [text('Get started')],
-                    ),
-                    a(
-                      classes: 'text-sm/6 font-semibold text-white',
-                      href: '#',
-                      [
-                        span(
-                          attributes: {
-                            'aria-hidden': 'true',
-                          },
-                          [text('Learn more '), text('→')],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
+        _buildContent(),
         _buildAppScreenshotImage(),
       ],
     );
@@ -188,6 +92,106 @@ class HeroSection extends AppStatelessComponent {
               'aspect-1108/632 w-277 bg-linear-to-r from-[#80caff] to-[#4f46e5] opacity-20',
           [], //todo add polygon
         )
+      ],
+    );
+  }
+
+  Component _buildContent() {
+    return div(
+      classes:
+          'mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:px-8 lg:py-40',
+      [
+        div(
+          classes: 'mx-auto max-w-2xl shrink-0 lg:mx-0 lg:pt-8',
+          [
+            img(
+              classes: 'h-11',
+              src:
+                  'https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500',
+              alt: 'Your Company',
+            ),
+            div(
+              classes: 'mt-30 sm:mt-32 lg:mt-16',
+              [
+                a(
+                  classes: 'inline-flex space-x-6',
+                  href: '#',
+                  [
+                    span(
+                      classes:
+                          'rounded-full bg-indigo-500/10 px-3 py-1 text-sm/6 font-semibold text-indigo-400 ring-1 ring-indigo-500/20 ring-inset',
+                      [
+                        text("What's new"),
+                      ],
+                    ),
+                    span(
+                      classes:
+                          'inline-flex items-center space-x-2 text-sm/6 font-medium text-gray-300',
+                      [
+                        span([text('Just shipped v1.0')]),
+                        svg(
+                          classes: 'size-5 text-gray-500',
+                          viewBox: '0 0 20 20',
+                          attributes: {
+                            'fill': 'currentColor',
+                            'aria-hidden': 'true',
+                            'data-slot': 'icon',
+                          },
+                          [
+                            path(
+                              attributes: {
+                                'fill-rule': 'evenodd',
+                                'clip-rule': 'evenodd',
+                              },
+                              d: 'M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z',
+                              [],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            p(
+              classes:
+                  'mt-10 text-5xl font-semibold tracking-tight text-pretty text-white sm:text-7xl',
+              [text('Deploy to the cloud with confidence')],
+            ),
+            p(
+              classes:
+                  'mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8',
+              [
+                text(
+                    'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.')
+              ],
+            ),
+            div(
+              classes: 'mt-10 flex items-center gap-x-6',
+              [
+                a(
+                  classes:
+                      'rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400',
+                  href: '#',
+                  [text('Get started')],
+                ),
+                a(
+                  classes: 'text-sm/6 font-semibold text-white',
+                  href: '#',
+                  [
+                    span(
+                      attributes: {
+                        'aria-hidden': 'true',
+                      },
+                      [text('Learn more '), text('→')],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ],
     );
   }
